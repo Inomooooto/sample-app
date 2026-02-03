f08308f750ff518a102148ce2a2e608b148833d8
Inomooooto <u-cp-love@i.softbank.jp>
2026-02-03 15:17:07 +0900

    Breezeの導入

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
         react(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
-    // vite.config.js
-    export default {
-        server: {
-            host: '127.0.0.1',
-            port: 5173 // 必要であればポートも固定
-        }
-    }
-
 });
