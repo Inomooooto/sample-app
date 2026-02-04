277dbc616887ed898c77fa7abf012df0973c0542
Inomooooto <u-cp-love@i.softbank.jp>
2026-02-04 15:57:44 +0900

    breezeをreactからbladeへ変更

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TaskController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -25,9 +17,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware('auth')->group(function () {
-    //タスクのルート(index,show,create,store,update,edit,destroy)
-    Route::resource('tasks', TaskController::class);
-});
-
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
